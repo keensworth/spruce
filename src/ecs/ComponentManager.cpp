d9fe4e1187ddc57caeaeca70810187b347bee1a3
keensworth <benjakeen@gmail.com>
2022-06-15 20:20:27 -0500

    add component manager

---
+++ src/ecs/ComponentManager.cpp
@@ -0,0 +1,103 @@
+#include "ComponentManager.h"
+
+namespace spr{
+
+ComponentManager::ComponentManager(){
+    m_components = std::vector<Component*>(64);
+}
+
+void ComponentManager::addComponent(Component& component){
+    m_components.push_back(&component);
+}
+
+template <typename T>
+bool typeMatch(auto data){
+    return typeid(data)==typeid(T);
+}
+
+template <typename T>
+auto ComponentManager::getEntityComponent(Entity entity){
+    auto val = std::find_if(m_components.begin(), m_components.end(), typeMatch<T>());
+    int index = -1;
+    if (val != m_components.end()){
+        index = std::distance(m_components.begin(), val);
+        return m_components.at(index).get(entity);
+    } else {
+        return nullptr;
+    }
+}
+
+template <typename T>
+void ComponentManager::setEntityComponent(Entity entity, auto data){
+    auto val = std::find_if(m_components.begin(), m_components.end(), typeMatch<T>());
+    int index = -1;
+    if (val != m_components.end()){
+        index = std::distance(m_components.begin(), val);
+        m_components.at(index).set(entity, data);
+    }
+}
+
+template <typename T>
+void ComponentManager::addComponentData(auto data){
+    auto val = std::find_if(m_components.begin(), m_components.end(), typeMatch<T>());
+    int index = -1;
+    if (val != m_components.end()){
+        index = std::distance(m_components.begin(), val);
+        m_components.at(index).add(data);
+    }
+}
+
+template <typename T>
+void ComponentManager::addComponentEntityData(Entity entity, auto data){
+    auto val = std::find_if(m_components.begin(), m_components.end(), typeMatch<T>());
+    int index = -1;
+    if (val != m_components.end()){
+        index = std::distance(m_components.begin(), val);
+        m_components.at(index).add(data);
+        m_components.at(index).addEntity(entity);
+    }
+}
+
+template <typename T>
+void ComponentManager::removeComponentEntity(Entity entity){
+    auto val = std::find_if(m_components.begin(), m_components.end(), typeMatch<T>());
+    int index = -1;
+    if (val != m_components.end()){
+        index = std::distance(m_components.begin(), val);
+        m_components.at(index).removeEntity(entity);
+    }
+}
+
+template <typename Arg, typename ...Args>
+long ComponentManager::getMask(){
+    long mask = 0L;
+    auto val = std::find_if(m_components.begin(), m_components.end(), typeMatch<T>());
+    int index = -1;
+    if (val != m_components.end()){
+        index = std::distance(m_components.begin(), val);
+        mask |= 0b1 << index;
+        return (mask | getMask<Args>());
+    } else {
+        return getMask<Args>();
+    }
+}
+
+long ComponentManager::getMask(std::vector<Component&> components){
+    long mask = 0L;
+    // for each component arg
+    for (int i = 0; i < components.size(); i++){
+        // for each registered component
+        int j = 0;
+        for (j = 0; j < m_components.size(); j++){
+            // if components match, break
+            if (typeid(components.at(i)) == typeid(m_components.at(j)))
+                break;
+            
+        }
+        // set mask bit relative to reg comp index
+        if (j < m_components.size())
+            mask |= (0b1 << j);
+    }
+    return mask;
+}
+}
---
+++ src/ecs/ComponentManager.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <vector>
+#include <Component.h>
+#include <Entity.h>
+#include <algorithm> 
+
+
+namespace spr {
+class ComponentManager{
+public:
+    ComponentManager();
+    ~ComponentManager() {}
+
+    // add a component
+    void addComponent(Component& component);
+
+    // get entity data for component T
+    template <typename T>
+    auto getEntityComponent(Entity entity);
+
+    // set data for component T, associated with entity
+    template <typename T>
+    void setEntityComponent(Entity entity, auto data);
+
+    // add data for component T
+    template <typename T>
+    void addComponentData(auto data);
+
+    // add data for component T, associated with entity
+    template <typename T>
+    void addComponentEntityData(Entity entity, auto data);
+
+    // remove component T associated with entity
+    template <typename T>
+    void removeComponentEntity(Entity entity);
+
+    // get bit mask for arg components
+    template <typename Arg, typename ...Args>
+    long getMask();
+
+    // get bit mask for components vector
+    long getMask(std::vector<Component&> components);
+
+    // register entity with set of components
+    void registerEntity(Entity entity, std::vector<Component&> components);
+
+    // register entity with set of components
+    void unregisterEntity(Entity entity);
+
+
+private:
+    std::vector<Component*> m_components;
+
+    // getMask variadic base case
+    long getMask(){
+        return 0L;
+    }
+};
+}
