f17b7b739bb0354e4f10de14186a301f12e3f973
keensworth <benjakeen@gmail.com>
2022-06-16 00:16:23 -0500

    add function definitions to component base class

--- src/ecs/Component.h
+++ src/ecs/Component.h
@@ -5,7 +5,23 @@
 
 namespace spr {
 
-class Component {};
+class Component {
+public:
+    // get entity's component data
+    auto get(Entity entity);
+
+    // set entity's component data
+    void set(Entity entity, auto data);
+
+    // add a new piece of data to container
+    void add(auto data);
+
+    // add entity to registry
+    void addEntity(Entity entity);
+
+    // remove entity from registry
+    void removeEntity(Entity entity);
+};
 
 template <typename T>
 class TypedComponent : public Component{
