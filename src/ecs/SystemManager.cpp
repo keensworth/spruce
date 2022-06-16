fc6e02f512c1b92cbbaf9b73ad43eac5c8b8e751
keensworth <benjakeen@gmail.com>
2022-06-15 20:21:08 -0500

    add system manager

---
+++ src/ecs/SystemManager.cpp
@@ -0,0 +1,32 @@
+#include "SystemManager.h"
+
+namespace spr {
+    
+SystemManager::SystemManager() {}
+
+void SystemManager::addSystem(System& system){
+    m_systems.push_back(&system);
+}
+
+void SystemManager::setRenderSystem(System& renderSystem){
+    m_renderSystem = &renderSystem;
+}
+
+void SystemManager::setAudioSystem(System& audioSystem){
+    m_audioSystem = &audioSystem;
+}
+
+void SystemManager::update(float dt){
+    // update systems
+    for(System* system : m_systems){
+        system->update(dt);
+    }
+
+    // update render system
+    m_renderSystem->update(dt);
+
+    // uppdate audio system
+    m_audioSystem->update(dt);
+}
+
+} // namespace spr
---
+++ src/ecs/SystemManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include <Component.h>
+#include <Entity.h>
+#include <System.h>
+
+
+namespace spr {
+class SystemManager{
+public:
+    SystemManager();
+    ~SystemManager() {}
+
+    // add a system
+    void addSystem(System& system);
+
+    // set the render sytem
+    void setRenderSystem(System& renderSystem);
+
+    // set the audio system
+    void setAudioSystem(System& audioSystem);
+
+    // update all systems sequentially
+    void update(float dt);
+
+private:
+    std::vector<System*> m_systems;
+    System* m_renderSystem;
+    System* m_audioSystem;
+
+    static System m_defaultSys;
+};
+}
