65eb1f9e4834d677ca67badbfe87208d31edf24e
keensworth <benjakeen@gmail.com>
2022-06-20 17:53:05 -0500

    add templated return type, virtualize base component class

--- src/ecs/Component.h
+++ src/ecs/Component.h
@@ -17,38 +17,59 @@ public:
     void add(auto data);
 
     // add entity to registry
-    void addEntity(Entity entity);
+    virtual void addEntity(Entity entity) = 0;
 
     // remove entity from registry
-    void removeEntity(Entity entity);
+    virtual void removeEntity(Entity entity) = 0;
 };
 
 template <typename T>
 class TypedComponent : public Component{
 public:
-    TypedComponent();
-    ~TypedComponent() {}
+    TypedComponent(){
+        m_container = Container<T>();
+        m_reg = Registry(512);
+        explicitInit = true;
+    }
+
+    ~TypedComponent(){}
 
     // get entity's component data
-    T get(Entity entity);
+    T get(Entity entity) {
+        return m_container.get(m_reg.getIndex(entity));
+    }
 
     // set entity's component data
-    void set(Entity entity, T data);
+    void set(Entity entity, T data){
+        m_container.set(m_reg.getIndex(entity), data);
+    }
 
     // add a new piece of data to container
-    void add(T data);
+    void add(T data){
+        m_container.add(data);
+    }
 
     // add entity to registry
-    void addEntity(Entity entity);
+    void addEntity(Entity entity){
+        m_reg.addItem(entity.id, m_container.lastWriteIndex);
+    }
 
     // remove entity from registry
-    void removeEntity(Entity entity);
+    void removeEntity(Entity entity){
+        // free container slot
+        m_container.erase(m_reg.getIndex(entity.id));
+    }
+
+    int getSize(){
+        return m_reg.getSize();
+    }
 
 private:
     // Per entity data
     Container<T> m_container;
     // Entity -> index map
     Registry m_reg;
+    bool explicitInit = false;
 };
 
 }
