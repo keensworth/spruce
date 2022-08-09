4036d5cc448ca7352acc4073538c81cf37a7e13e
keensworth <benjakeen@gmail.com>
2022-08-08 22:39:31 -0500

    add SDL input handler

---
+++ src/interface/InputHandler.cpp
@@ -0,0 +1,91 @@
+#include "InputHandler.h"
+
+namespace spr {
+InputHandler::InputHandler(){
+    keyboard = new KeyboardState();
+    mouse = new MouseState();
+    inputManager = InputManager(keyboard, mouse);
+}
+
+InputManager* InputHandler::getInputManager(){
+    return &inputManager;
+}
+
+void InputHandler::handleKeyPress(bool keyDown){
+    SDL_Keycode keycode = event.key.keysym.sym;
+    SprKey key = getSprKeyFromSDLKeycode(keycode);
+    keyboard->keyDown[key] = keyDown;
+    if (keyDown)
+        keyboard->keyDownTicks[key] = SDL_GetTicks();
+    else 
+        keyboard->keyUpTicks[key] = SDL_GetTicks();
+}
+
+void InputHandler::handleButtonPress(bool buttonDown){
+    int button = event.button.button;
+    SprButton sprButton = getSprButtonFromSDLButton(button);
+    mouse->buttonDown[sprButton] = buttonDown;
+    if (buttonDown)
+        mouse->buttonDownTicks[sprButton] = SDL_GetTicks();
+    else 
+        mouse->buttonUpTicks[sprButton] = SDL_GetTicks();
+}
+
+void InputHandler::handleMouseMotion(){
+    ivec2 mousePos;
+    mousePos.x = event.motion.x;
+    mousePos.y = event.motion.y;
+
+    ivec2 mouseMotion;
+    mouseMotion.x = event.motion.xrel;
+    mouseMotion.y = event.motion.yrel;
+
+    mouse->mousePos = mousePos;
+    mouse->mouseMotion = mouseMotion;
+    mouse->mouseMotionTicks = SDL_GetTicks();
+}
+
+void InputHandler::handleMouseWheel(){
+    ivec2 scrollMotion;
+    scrollMotion.x = event.wheel.x;
+    scrollMotion.y = event.wheel.y;
+
+    mouse->scrollWheelMotion = scrollMotion;
+    mouse->scrollTicks = SDL_GetTicks();
+}
+
+void InputHandler::update(){
+    while (SDL_PollEvent(&event)){
+        switch( event.type ){
+            // handle keyboard input
+            case SDL_KEYDOWN:
+                handleKeyPress(true);
+                break;
+            case SDL_KEYUP:
+                handleKeyPress(false);
+                break;
+
+            // handle mouse button input
+            case SDL_MOUSEBUTTONDOWN:
+                handleButtonPress(true);
+                break;
+            case SDL_MOUSEBUTTONUP:
+                handleButtonPress(false);
+                break;
+
+            // handle mouse wheel input
+            case SDL_MOUSEWHEEL:
+                handleMouseWheel();
+                break;
+            
+            // handle mouse motion
+            case SDL_MOUSEMOTION:
+                handleMouseMotion();
+                break;
+
+            default:
+                break;
+        }   
+    }   
+}
+}
--- src/interface/InputHandler.h
+++ src/interface/InputHandler.h
@@ -1,12 +1,29 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include "InputManager.h"
 
 namespace spr {
 class InputHandler {
 public:
+    InputHandler();
+    ~InputHandler(){
+        delete keyboard;
+        delete mouse;
+    }
+    
     void update();
+    InputManager* getInputManager();
+
 private:
     InputManager inputManager;
+    KeyboardState* keyboard;
+    MouseState* mouse;
+    SDL_Event event;
+
+    void handleKeyPress(bool keyDown);
+    void handleButtonPress(bool buttonDown);
+    void handleMouseMotion();
+    void handleMouseWheel();
 };
 }
