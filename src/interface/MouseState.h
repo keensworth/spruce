75bd0868c7655b72e6419d1402ad990d652a3949
keensworth <benjakeen@gmail.com>
2022-08-08 22:41:27 -0500

    make all members public, index buttons by SprButton

--- src/interface/MouseState.h
+++ src/interface/MouseState.h
@@ -10,17 +10,16 @@ class MouseState {
 public:
     MouseState() {
         mousePos = ivec2(0,0);
-        mouseMovement = ivec2(0,0);
-        scrollWheelMovement = ivec2(0,0);
+        mouseMotion = ivec2(0,0);
+        scrollWheelMotion = ivec2(0,0);
 
-        buttonDownTicks = 0;
-        buttonUpTicks = 0;
-        mouseMoveTicks = 0;
-        scrollUpTicks = 0;
-        scrollDownTicks = 0;
+        mouseMotionTicks = 0;
+        scrollTicks = 0;
 
         for (int i = 0; i < BUTTON_COUNT; i++){
             buttonDown[i] = false;
+            buttonDownTicks[i] = 0;
+            buttonUpTicks[i] = 0;
         }
     }
 
@@ -28,15 +27,14 @@ public:
 
     // mouse state
     ivec2 mousePos;
-    ivec2 mouseMovement;
-    ivec2 scrollWheelMovement;
+    ivec2 mouseMotion;
+    ivec2 scrollWheelMotion;
     bool buttonDown[BUTTON_COUNT];
 
     // mouse timing
-    int buttonDownTicks;
-    int buttonUpTicks;
-    int mouseMoveTicks;
-    int scrollUpTicks;
-    int scrollDownTicks;
+    int buttonDownTicks[BUTTON_COUNT];
+    int buttonUpTicks[BUTTON_COUNT];
+    int mouseMotionTicks;
+    int scrollTicks;
 };
 }
