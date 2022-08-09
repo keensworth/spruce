c224ad5752109a8e1b35fe9fdca3ce683391d147
keensworth <benjakeen@gmail.com>
2022-08-08 22:41:50 -0500

    update InputManager interface

--- src/interface/InputManager.h
+++ src/interface/InputManager.h
@@ -1,12 +1,32 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include "KeyboardState.h"
+#include "MouseState.h"
 
 namespace spr {
 class InputManager {
 public:
-    //
+    InputManager();
+    InputManager(KeyboardState* keyboard, MouseState* mouse);
+    ~InputManager(){}
+
+    bool isKeyDown(SprKey key);
+    bool isButtonDown(SprButton key);
+    ivec2 getMousePos();
+    ivec2 getMouseMovement();
+    ivec2 getScrollWheelMovement();
+
+    int timeSinceKeyDown(SprKey key);
+    int timeSinceKeyUp(SprKey key);
+    int timeSinceButtonDown(SprButton button);
+    int timeSinceButtonUp(SprButton button);
+    int timeSinceMouseMove();
+    int timeSinceScrollUp();
+    int timeSinceScrollDown();
+    
 private:
-    //
+    KeyboardState* keyboard;
+    MouseState* mouse;
 };
 }
