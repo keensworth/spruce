cf852c8b1705b94e89353f0516a9da537247f347
keensworth <benjakeen@gmail.com>
2022-10-26 13:26:07 -0500

    ResourceCache instantiation and init

--- src/resource/ResourceManager.cpp
+++ src/resource/ResourceManager.cpp
@@ -6,6 +6,76 @@ ResourceManager::ResourceManager(){
     m_resourceCaches = std::vector<ResourceCache*>();
     m_resourceMap = rmap();
     m_resourceIndex = 0;
+
+    // add resource types and store pointers to caches
+    addResourceType<ModelCache>(new ModelCache);
+    addResourceType<MeshCache>(new MeshCache);
+    addResourceType<MaterialCache>(new MaterialCache);
+    addResourceType<TextureCache>(new TextureCache);
+    addResourceType<BufferCache>(new BufferCache);
+}
+
+ResourceManager::~ResourceManager(){
+    // delete model cache
+    uint32 index = getResourceCacheIndex<ModelCache>();
+    ModelCache* modelCache = ((ModelCache*) dynamic_cast<ModelCache*>(m_resourceCaches.at(index)));
+    delete modelCache;
+
+    // delete mesh cache
+    index = getResourceCacheIndex<MeshCache>();
+    MeshCache* meshCache = ((MeshCache*) dynamic_cast<MeshCache*>(m_resourceCaches.at(index)));
+    delete meshCache;
+
+    // delete material cache
+    index = getResourceCacheIndex<MaterialCache>();
+    MaterialCache* materialCache = ((MaterialCache*) dynamic_cast<MaterialCache*>(m_resourceCaches.at(index)));
+    delete materialCache;
+
+    // delete texture cache
+    index = getResourceCacheIndex<TextureCache>();
+    TextureCache* textureCache = ((TextureCache*) dynamic_cast<TextureCache*>(m_resourceCaches.at(index)));
+    delete textureCache;
+
+    // delete buffer cache
+    index = getResourceCacheIndex<BufferCache>();
+    BufferCache* bufferCache = ((BufferCache*) dynamic_cast<BufferCache*>(m_resourceCaches.at(index)));
+    delete bufferCache;
+}
+
+void ResourceManager::init(){
+    // load resource metadata from asset_manifest.json
+    AssetLoader assetLoader;
+    std::vector<ResourceMetadata> resourceMetadata = assetLoader.loadMetadata();
+
+    // register resources with metadata
+    for (auto & metadata : resourceMetadata){
+        switch(metadata.resourceType) {
+            case SPR_MESH :
+                registerResource<MeshCache>(metadata);
+                break;
+            case SPR_MODEL :
+                registerResource<ModelCache>(metadata);
+                break;
+            case SPR_AUDIO :
+                //registerResource<AudioCache>(metadata);
+                break;
+            case SPR_SHADER :
+                //registerResource<ShaderCache>(metadata);
+                break;
+            case SPR_BUFFER :
+                registerResource<BufferCache>(metadata);
+                break;
+            case SPR_TEXTURE :
+                registerResource<TextureCache>(metadata);
+                break;
+            case SPR_MATERIAL :
+                registerResource<MaterialCache>(metadata);
+                break;
+            default:
+                // log error
+                break;
+        }
+    }
 }
 
 }
--- src/resource/ResourceManager.h
+++ src/resource/ResourceManager.h
@@ -7,37 +7,29 @@
 #pragma once
 
 #include "ResourceCache.h"
+#include "AssetLoader.h"
 
 
 typedef std::unordered_map<std::type_index, uint32> rmap;
 
 namespace spr {
+
+class ModelCache : public TypedResourceCache<Model>{};
+class MeshCache : public TypedResourceCache<Mesh>{};
+class MaterialCache : public TypedResourceCache<Material>{};
+class TextureCache : public TypedResourceCache<Texture>{};
+class BufferCache : public TypedResourceCache<Buffer>{};
+
 class ResourceManager {
 public:
     ResourceManager();
-    ~ResourceManager(){}
-
-    template <typename T>
-    void addResourceType(ResourceCache& resourceCache){
-        m_resourceCaches.push_back(&resourceCache);
-        m_resourceMap[typeid(T)] = m_resourceIndex;
-        m_resourceMap[typeid(T*)] = m_resourceIndex;
-        m_resourceIndex++;
-    }
+    ~ResourceManager();
 
     // T := ResourceCache
     template <typename T>
     uint32 getResourceCacheIndex(){
         uint32 index = m_resourceMap[typeid(T)];
         return index;
-    }
-
-    // T := ResourceCache
-    template <typename T>
-    void registerResource(ResourceMetadata metadata){
-        uint32 index = getResourceCacheIndex<T>();
-        T* resourceCache = ((T*) dynamic_cast<T*>(m_resourceCaches.at(index)));
-        resourceCache->registerResource(metadata);
     }
 
     // T := ResourceCache
@@ -56,10 +48,29 @@ public:
         T* resourceCache = ((T*) dynamic_cast<T*>(m_resourceCaches.at(index)));
         return resourceCache->getData(handle);
     }
-
+    
 private:
     std::vector<ResourceCache*> m_resourceCaches;
     rmap m_resourceMap;
     uint32 m_resourceIndex;
+
+    void init();
+
+    // T := ResourceCache
+    template <typename T>
+    void addResourceType(ResourceCache* resourceCache){
+        m_resourceCaches.push_back(resourceCache);
+        m_resourceMap[typeid(T)] = m_resourceIndex;
+        m_resourceMap[typeid(T*)] = m_resourceIndex;
+        m_resourceIndex++;
+    }
+
+    // T := ResourceCache
+    template <typename T>
+    void registerResource(ResourceMetadata metadata){
+        uint32 index = getResourceCacheIndex<T>();
+        T* resourceCache = ((T*) dynamic_cast<T*>(m_resourceCaches.at(index)));
+        resourceCache->registerResource(metadata);
+    }
 };
 }
