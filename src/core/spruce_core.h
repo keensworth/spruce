7363d82a58016512db2d71e4e5fb81343acf2b40
s3rgei <benjakeen@gmail.com>
2022-12-09 17:54:31 -0600

    rename Core.h

--- src/core/spruce_core.h
+++ src/core/spruce_core.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <glm/glm.hpp>
 
+
 // integer types
 typedef int8_t int8;
 typedef int16_t int16;
@@ -14,3 +15,4 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
+
