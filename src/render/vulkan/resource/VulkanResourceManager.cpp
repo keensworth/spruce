207f370ad5656ad05093353d7d0801183c8dab2c
s3rgei <benjakeen@gmail.com>
2022-12-16 19:29:23 -0600

    implementations for RenderPassLayout and RenderPass

--- src/render/vulkan/resource/VulkanResourceManager.cpp
+++ src/render/vulkan/resource/VulkanResourceManager.cpp
@@ -1,20 +1,23 @@
 #include "VulkanResourceManager.h"
+#include "ResourceFlags.h"
+#include "ResourceTypes.h"
+#include <vulkan/vulkan_core.h>
 
 namespace spr::gfx {
+
+
+//  ██╗███╗░░██╗██╗████████╗
+//  ██║████╗░██║██║╚══██╔══╝
+//  ██║██╔██╗██║██║░░░██║░░░
+//  ██║██║╚████║██║░░░██║░░░
+//  ██║██║░╚███║██║░░░██║░░░
+//  ╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░
 
 VulkanResourceManager::VulkanResourceManager(){
 
 }
 
 VulkanResourceManager::~VulkanResourceManager(){
-    // delete shader cache
-    ShaderCache* shaderCache = ((ShaderCache*) m_resourceMap[typeid(Shader)]);
-    delete shaderCache;
-
-    // delete pipeline cache
-    PipelineCache* pipelineCache = ((PipelineCache*) m_resourceMap[typeid(Pipeline)]);
-    delete pipelineCache;
-
     // delete buffer cache
     BufferCache* bufferCache = ((BufferCache*) m_resourceMap[typeid(Buffer)]);
     delete bufferCache;
@@ -22,10 +25,6 @@ VulkanResourceManager::~VulkanResourceManager(){
     // delete texture cache
     TextureCache* textureCache = ((TextureCache*) m_resourceMap[typeid(Texture)]);
     delete textureCache;
-
-    // delete mesh cache
-    MeshCache* meshCache = ((MeshCache*) m_resourceMap[typeid(Mesh)]);
-    delete meshCache;
 
     // delete descriptor set layout cache
     DescriptorSetLayoutCache* descriptorSetLayoutCache = ((DescriptorSetLayoutCache*) m_resourceMap[typeid(DescriptorSetLayout)]);
@@ -36,32 +35,29 @@ VulkanResourceManager::~VulkanResourceManager(){
     delete descriptorSet;
 
     // delete render pass layout cache
-    RenderPassLayoutCache* renderPassLaoutCache = ((RenderPassLayoutCache*) m_resourceMap[typeid(RenderPassLayout)]);
-    delete renderPassLaoutCache;
+    RenderPassLayoutCache* renderPassLayoutCache = ((RenderPassLayoutCache*) m_resourceMap[typeid(RenderPassLayout)]);
+    delete renderPassLayoutCache;
 
     // delete render pass cache
     RenderPassCache* renderPassCache = ((RenderPassCache*) m_resourceMap[typeid(RenderPass)]);
     delete renderPassCache;
 
-    // delete frame buffer cache
-    FrameBufferCache* frameBufferCache = ((FrameBufferCache*) m_resourceMap[typeid(FrameBuffer)]);
-    delete frameBufferCache;
-}
-
-template<>
-Handle<Shader> VulkanResourceManager::create<Shader>(ShaderDesc shaderDesc){
+    // delete shader cache
     ShaderCache* shaderCache = ((ShaderCache*) m_resourceMap[typeid(Shader)]);
-    Shader shader;
-    return shaderCache->insert(shader);
+    delete shaderCache;
 }
 
-template<>
-Handle<Pipeline> VulkanResourceManager::create<Pipeline>(PipelineDesc pipelineDesc){
-    PipelineCache* pipelineCache = ((PipelineCache*) m_resourceMap[typeid(Pipeline)]);
-    Pipeline pipeline;
-    return pipelineCache->insert(pipeline);
-}
 
+//  ░█████╗░██████╗░███████╗░█████╗░████████╗███████╗
+//  ██╔══██╗██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝
+//  ██║░░╚═╝██████╔╝█████╗░░███████║░░░██║░░░█████╗░░
+//  ██║░░██╗██╔══██╗██╔══╝░░██╔══██║░░░██║░░░██╔══╝░░
+//  ╚█████╔╝██║░░██║███████╗██║░░██║░░░██║░░░███████╗
+//  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝
+
+// ------------------------------------------------------------------------- //
+//                 Buffer                                                    // 
+// ------------------------------------------------------------------------- //
 template<>
 Handle<Buffer> VulkanResourceManager::create<Buffer>(BufferDesc bufferDesc){
     BufferCache* bufferCache = ((BufferCache*) m_resourceMap[typeid(Buffer)]);
@@ -69,6 +65,10 @@ Handle<Buffer> VulkanResourceManager::create<Buffer>(BufferDesc bufferDesc){
     return bufferCache->insert(buffer);
 }
 
+
+// ------------------------------------------------------------------------- //
+//                 Texture                                                   // 
+// ------------------------------------------------------------------------- //
 template<>
 Handle<Texture> VulkanResourceManager::create<Texture>(TextureDesc textureDesc){
     TextureCache* textureCache = ((TextureCache*) m_resourceMap[typeid(Texture)]);
@@ -76,20 +76,21 @@ Handle<Texture> VulkanResourceManager::create<Texture>(TextureDesc textureDesc){
     return textureCache->insert(texture);
 }
 
-template<>
-Handle<Mesh> VulkanResourceManager::create<Mesh>(MeshDesc meshDesc){
-    MeshCache* meshCache = ((MeshCache*) m_resourceMap[typeid(Mesh)]);
-    Mesh mesh;
-    return meshCache->insert(mesh);
-}
 
+// ------------------------------------------------------------------------- //
+//                 Descriptor Set Layout                                     // 
+// ------------------------------------------------------------------------- //
 template<>
-Handle<DescriptorSetLayout> VulkanResourceManager::create<DescriptorSetLayout>(DescriptorSetLayoutDesc descriptorSetLayoutDesc){
+Handle<DescriptorSetLayout> VulkanResourceManager::create<DescriptorSetLayout>(DescriptorSetLayoutDesc desc){
     DescriptorSetLayoutCache* descriptorSetLayoutCache = ((DescriptorSetLayoutCache*) m_resourceMap[typeid(DescriptorSetLayout)]);
     DescriptorSetLayout descriptorSetLayout;
     return descriptorSetLayoutCache->insert(descriptorSetLayout);
 }
 
+
+// ------------------------------------------------------------------------- //
+//                 Descriptor Set                                            // 
+// ------------------------------------------------------------------------- //
 template<>
 Handle<DescriptorSet> VulkanResourceManager::create<DescriptorSet>(DescriptorSetDesc descriptorSetDesc){
     DescriptorSetCache* descriptorSetCache = ((DescriptorSetCache*) m_resourceMap[typeid(DescriptorSet)]);
@@ -97,25 +98,161 @@ Handle<DescriptorSet> VulkanResourceManager::create<DescriptorSet>(DescriptorSet
     return descriptorSetCache->insert(descriptorSet);
 }
 
+
+// ------------------------------------------------------------------------- //
+//                 Render Pass Layout                                        // 
+// ------------------------------------------------------------------------- //
 template<>
-Handle<RenderPassLayout> VulkanResourceManager::create<RenderPassLayout>(RenderPassLayoutDesc renderPassLayoutDesc){
-    RenderPassLayoutCache* renderPassLaoutCache = ((RenderPassLayoutCache*) m_resourceMap[typeid(RenderPassLayout)]);
-    RenderPassLayout renderPassLayout;
-    return renderPassLaoutCache->insert(renderPassLayout);
+Handle<RenderPassLayout> VulkanResourceManager::create<RenderPassLayout>(RenderPassLayoutDesc desc){
+    RenderPassLayoutCache* renderPassLayoutCache = ((RenderPassLayoutCache*) m_resourceMap[typeid(RenderPassLayout)]);
+
+    // meta
+    bool depthAttachment = desc.subpass.depthAttachment;
+    uint32 attachmentCount = desc.colorAttatchmentFormats.size();
+
+    // create attachment descriptions
+    std::vector<VkAttachmentDescription> descriptions(attachmentCount);
+    {
+        // color 
+        for (int i = 0; i < attachmentCount; i++)
+            descriptions[i] = {
+                .format = (VkFormat)desc.colorAttatchmentFormats[i]
+            };
+        // depth 
+        if (depthAttachment)
+            descriptions[attachmentCount] = {
+                .format = (VkFormat)desc.depthAttachmentFormat
+            };
+    }
+
+    // create attachment references
+    VkAttachmentReference depthReference;
+    std::vector<VkAttachmentReference> colorReferences;
+    {
+        // color
+        for (uint32 colorAttachment : desc.subpass.colorAttachments)
+            colorReferences.push_back({
+                .attachment = colorAttachment,
+                .layout = (VkImageLayout)Flags::ImageLayout::COLOR_ATTACHMENT
+            });
+        
+        // depth
+        if (depthAttachment)
+            depthReference = {
+                .attachment = (uint32)attachmentCount,
+                .layout = (VkImageLayout)Flags::ImageLayout::DEPTH_STENCIL_ATTACHMENT
+            };
+    }
+
+    // build subpass description
+    VkSubpassDescription subpassDescription {
+        .pipelineBindPoint       = (VkPipelineBindPoint)Flags::BindPoint::BP_GRAPHICS,
+        .inputAttachmentCount    = 0,
+        .pInputAttachments       = NULL,
+        .colorAttachmentCount    = attachmentCount,
+        .pColorAttachments       = colorReferences.data(),
+        .pResolveAttachments     = NULL,
+        .pDepthStencilAttachment = depthAttachment ? &depthReference : NULL,
+        .preserveAttachmentCount = 0,
+        .pPreserveAttachments    = NULL
+    };
+
+    // build RenderPassLayout 
+    RenderPassLayout renderPassLayout {
+        .depthReference         = depthReference,
+        .colorReferences        = colorReferences,
+        .attachmentDescriptions = descriptions,
+        .subpassDescription     = subpassDescription,
+        .attachmentCount        = (uint32)attachmentCount + (depthAttachment ? 1 : 0)
+    };
+    return renderPassLayoutCache->insert(renderPassLayout);
 }
 
+
+// ------------------------------------------------------------------------- //
+//                 Render Pass                                               // 
+// ------------------------------------------------------------------------- //
 template<>
-Handle<RenderPass> VulkanResourceManager::create<RenderPass>(RenderPassDesc renderPassDesc){
+Handle<RenderPass> VulkanResourceManager::create<RenderPass>(RenderPassDesc desc){
     RenderPassCache* renderPassCache = ((RenderPassCache*) m_resourceMap[typeid(RenderPass)]);
-    RenderPass renderPass;
+
+    // layout data from handle in desc
+    RenderPassLayout* layout = get<RenderPassLayout>(desc.layout);
+    
+    // insert new description info into descriptions
+    bool depthAttachment = (layout->colorReferences.size() + 1) == layout->attachmentCount;
+    // depth
+    if (depthAttachment) {
+        VkAttachmentDescription newAttachment = {
+            .format         = layout->attachmentDescriptions[layout->attachmentCount-1].format,
+            .samples        = (VkSampleCountFlagBits)desc.depthAttachment.samples,
+            .loadOp         = (VkAttachmentLoadOp)desc.depthAttachment.loadOp,
+            .storeOp        = (VkAttachmentStoreOp)desc.depthAttachment.storeOp,
+            .stencilLoadOp  = (VkAttachmentLoadOp)desc.depthAttachment.stencilLoadOp,
+            .stencilStoreOp = (VkAttachmentStoreOp)desc.depthAttachment.stencilStoreOp,
+            .initialLayout  = (VkImageLayout)desc.depthAttachment.layout,
+            .finalLayout    = (VkImageLayout)desc.depthAttachment.finalLayout
+        };
+        layout->attachmentDescriptions[layout->attachmentCount-1] = newAttachment;
+    }
+    // color
+    int attachmentIndex = 0;
+    for (auto attachment : desc.colorAttachments){
+        VkAttachmentDescription newAttachment = {
+            .format         = layout->attachmentDescriptions[attachmentIndex].format,
+            .samples        = (VkSampleCountFlagBits)attachment.samples,
+            .loadOp         = (VkAttachmentLoadOp)attachment.loadOp,
+            .storeOp        = (VkAttachmentStoreOp)attachment.storeOp,
+            .stencilLoadOp  = (VkAttachmentLoadOp)attachment.stencilLoadOp,
+            .stencilStoreOp = (VkAttachmentStoreOp)attachment.stencilStoreOp,
+            .initialLayout  = (VkImageLayout)attachment.layout,
+            .finalLayout    = (VkImageLayout)attachment.finalLayout
+        };
+        layout->attachmentDescriptions[attachmentIndex] = newAttachment;
+        attachmentIndex++;
+    }
+    
+    // build render pass create info
+    VkRenderPassCreateInfo createInfo {
+        .sType           = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+        .attachmentCount = layout->attachmentCount,
+        .pAttachments    = layout->attachmentDescriptions.data(),
+        .subpassCount    = 1,
+        .pSubpasses      = &(layout->subpassDescription),
+        .dependencyCount = 0,
+        .pDependencies   = NULL
+    };
+
+    // create vulkan render pass
+    VkRenderPass vulkanRenderPass;
+    VK_CHECK(vkCreateRenderPass(m_device, &createInfo, NULL, &vulkanRenderPass));
+
+    // build RenderPass
+    RenderPass renderPass {
+        .layout = desc.layout,
+        .renderPass = vulkanRenderPass
+    };
     return renderPassCache->insert(renderPass);
 }
 
+
+// ------------------------------------------------------------------------- //
+//                 Shader                                                    // 
+// ------------------------------------------------------------------------- //
 template<>
-Handle<FrameBuffer> VulkanResourceManager::create<FrameBuffer>(FrameBufferDesc frameBufferDesc){
-    FrameBufferCache* frameBufferCache = ((FrameBufferCache*) m_resourceMap[typeid(FrameBuffer)]);
-    FrameBuffer frameBuffer;
-    return frameBufferCache->insert(frameBuffer);
+Handle<Shader> VulkanResourceManager::create<Shader>(ShaderDesc shaderDesc){
+    ShaderCache* shaderCache = ((ShaderCache*) m_resourceMap[typeid(Shader)]);
+    Shader shader;
+    return shaderCache->insert(shader);
 }
+
+
+//  ██████╗░███████╗███╗░░░███╗░█████╗░██╗░░░██╗███████╗
+//  ██╔══██╗██╔════╝████╗░████║██╔══██╗██║░░░██║██╔════╝
+//  ██████╔╝█████╗░░██╔████╔██║██║░░██║╚██╗░██╔╝█████╗░░
+//  ██╔══██╗██╔══╝░░██║╚██╔╝██║██║░░██║░╚████╔╝░██╔══╝░░
+//  ██║░░██║███████╗██║░╚═╝░██║╚█████╔╝░░╚██╔╝░░███████╗
+//  ╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░╚══════╝
+
 
 }
