bc6c7d20c4c1a88c807ee34dfe8975cdac9cba06
s3rgei <benjakeen@gmail.com>
2023-02-17 14:28:08 -0600

    add additional scene info

--- src/render/scene/SceneData.h
+++ src/render/scene/SceneData.h
@@ -4,6 +4,18 @@
 
 namespace spr::gfx {
 
+typedef struct Transform {
+    glm::mat4 model;
+    glm::mat4 modelInvTranspose;
+} Transform;
+
+typedef struct MeshData {
+    uint32 vertexOffset;
+    uint32 indexCount;
+    uint32 materialIndex;
+    uint32 padding;
+} MeshData;
+
 typedef enum LightTypes {
     POINT,
     SPOT,
@@ -32,5 +44,11 @@ typedef struct Camera {
     glm::vec3 up;
     float far;
 } Camera;
+
+typedef struct Scene {
+    glm::mat4x4 view;
+    glm::mat4x4 proj;
+    Camera camera;
+} Scene;
 
 }
