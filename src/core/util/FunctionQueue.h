9fa8c491b189c1fd87592730049026051e5ddefd
s3rgei <benjakeen@gmail.com>
2023-02-17 14:28:48 -0600

    add function queue, used for cmd batching

---
+++ src/core/util/FunctionQueue.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <deque>
+#include <vector>
+#include <functional>
+
+namespace spr {
+
+typedef struct FunctionQueue {
+    std::deque<std::function<void()>> functors;
+
+    void push_function(std::function<void()>&& function) {
+        functors.push_back(function);
+    }
+
+    void execute() {
+        for (auto it = functors.rbegin(); it != functors.rend(); it++) {
+            (*it)(); 
+        }
+
+        functors.clear();
+    }
+} FunctionQueue;
+
+}
