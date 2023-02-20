ba5c204cf98a662f5680daf60f564c3aac2e1a77
s3rgei <benjakeen@gmail.com>
2023-02-20 04:59:01 -0600

    some defaults for scene objects

--- src/render/scene/SceneData.h
+++ src/render/scene/SceneData.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "spruce_core.h"
+#include <numbers>
 
 namespace spr::gfx {
 
@@ -16,33 +17,34 @@ typedef struct MeshData {
     uint32 padding;
 } MeshData;
 
-typedef enum LightTypes {
+typedef enum LightType {
     POINT,
     SPOT,
     DIRECTIONAL
-} LightTypes;
+} LightType;
 
 typedef struct SpotProperties {
-    float innerAngle;
-    float outerAngle;
+    float innerAngle = 3.1415f/4.f;
+    float outerAngle = 3.1415f/3.f;
 } SpotProperties;
 
 typedef struct Light {
-    glm::vec3 pos;
-    uint32 type;
-    glm::vec3 dir;
-    float intensity;
-    glm::vec3 color;
-    float range;
+    glm::vec3 pos = {0.f, 0.f, 0.f};
+    LightType type = POINT;
+    glm::vec3 dir = {0.f, 1.f, 0.f};
+    float intensity = 1.f;
+    glm::vec3 color = {0.f, 0.f, 0.f};
+    float range = 16.f;
+    SpotProperties spotProps;
 } Light;
 
 typedef struct Camera {
-    glm::vec3 pos;
-    float fov;
-    glm::vec3 dir;
-    float near;
-    glm::vec3 up;
-    float far;
+    glm::vec3 pos = {0.f, 0.f, 0.f};
+    float fov = 3.1415f/3.f;
+    glm::vec3 dir = {0.f, 1.f, 0.f};
+    float near = 0.01f;
+    glm::vec3 up = {0.f, 0.f, 1.f};
+    float far = 1024.f;
 } Camera;
 
 typedef struct Scene {
