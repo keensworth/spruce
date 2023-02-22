2157ee47400224ed904ce97abd2813347614a3b0
s3rgei <benjakeen@gmail.com>
2023-02-21 21:28:01 -0600

    initial scene data values

--- src/render/scene/SceneData.h
+++ src/render/scene/SceneData.h
@@ -29,28 +29,27 @@ typedef struct SpotProperties {
 } SpotProperties;
 
 typedef struct Light {
-    glm::vec3 pos = {0.f, 0.f, 0.f};
-    LightType type = POINT;
-    glm::vec3 dir = {0.f, 1.f, 0.f};
+    glm::vec3 pos   = {0.f, 0.f, 0.f};
     float intensity = 1.f;
-    glm::vec3 color = {0.f, 0.f, 0.f};
-    float range = 16.f;
+    glm::vec3 dir   = {0.f, 1.f, 0.f};
+    float range     = 16.f;
+    glm::vec3 color = {1.f, 1.f, 1.f};
     SpotProperties spotProps;
+    LightType type  = POINT;
 } Light;
 
 typedef struct Camera {
     glm::vec3 pos = {0.f, 0.f, 0.f};
-    float fov = 3.1415f/3.f;
+    float fov     = 3.1415f/3.f;
     glm::vec3 dir = {0.f, 1.f, 0.f};
-    float near = 0.01f;
-    glm::vec3 up = {0.f, 0.f, 1.f};
-    float far = 1024.f;
+    float near    = 0.01f;
+    glm::vec3 up  = {0.f, 0.f, 1.f};
+    float far     = 1024.f;
 } Camera;
 
 typedef struct Scene {
     glm::mat4x4 view;
     glm::mat4x4 proj;
-    Camera camera;
 } Scene;
 
 }
