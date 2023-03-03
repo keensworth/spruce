338c0766a96f78a936454ebbf5f3b912a284edee
s3rgei <benjakeen@gmail.com>
2023-03-02 21:58:05 -0600

    add scene data buffer members, and isolate renderer members

--- src/render/RenderCoordinator.h
+++ src/render/RenderCoordinator.h
@@ -2,6 +2,8 @@
 
 #include "scene/SceneManager.h"
 #include "vulkan/VulkanRenderer.h"
+#include "vulkan/resource/ResourceTypes.h"
+#include "vulkan/FrameRenderer.h"
 
 
 
@@ -18,10 +20,27 @@ public:
 
 private:
     Window* m_window;
-
     VulkanResourceManager m_rm;
     VulkanRenderer m_renderer;
 
-    void init();
+    uint32 m_frame = 0;
+
+    Handle<Buffer> m_lightsBuffer;
+    Handle<Buffer> m_transformBuffer;
+    Handle<Buffer> m_drawDataBuffer;
+    Handle<Buffer> m_cameraBuffer;
+    Handle<Buffer> m_sceneBuffer;
+
+    const uint32 MAX_DRAWS = 1<<17; // 2^18 draws
+
+    void initBuffers();
+    void initRenderers();
+    void uploadResources(SceneManager& sceneManager);
+
+private:
+    // MAIN renderer
+    FrameRenderer m_frameRenderer;
+
+    // OFFSCREEN renderers
 };
 }
