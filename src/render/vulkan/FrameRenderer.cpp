fdb69bedf084eec849efccc5827dae8e9ace92e0
s3rgei <benjakeen@gmail.com>
2023-03-09 03:40:59 -0600

    update FrameRenderer initialization

--- src/render/vulkan/FrameRenderer.cpp
+++ src/render/vulkan/FrameRenderer.cpp
@@ -1,43 +1,84 @@
 #include "FrameRenderer.h"
 #include "resource/ResourceFlags.h"
+#include "resource/ResourceTypes.h"
 
 namespace spr::gfx {
 
-FrameRenderer::FrameRenderer(VulkanResourceManager& rm, VkFormat displayFormat, glm::vec3 dimensions, Handle<Texture> result){
+FrameRenderer::FrameRenderer(){}
 
-    m_renderPassLayout = rm.create<RenderPassLayout>((RenderPassLayoutDesc){
-        .colorAttatchmentFormats = {displayFormat},
-        .subpass = {
-            .colorAttachments = {0}
-        }
-    });
 
-    m_renderPass = rm.create<RenderPass>((RenderPassDesc){
-        .dimensions = dimensions,
-        .layout = m_renderPassLayout,
-        .colorAttachments = {
-            {.texture = Handle<TextureAttachment>()} // TODO: insert handle wrapper of swapchain image
-        }
-    });
-
-    // TODO: need to create descriptor sets + layouts (pass in global ones?)
-    //       use result tex handle to set tex binding
-
-    m_shader = rm.create<Shader>((ShaderDesc){
-        .vertexShader = {.shaderPath = ""},
-        .fragmentShader = {.shaderPath = ""},
-        .descriptorSets = {},
-        .graphicsState = {
-            .renderPass = m_renderPass
-        }
-    });
+FrameRenderer::FrameRenderer(VulkanResourceManager& rm, VulkanRenderer& renderer, glm::uvec3 dimensions){
+    m_rm = &rm;
+    m_renderer = &renderer;
+    m_dim = dimensions;
 }
 
 FrameRenderer::~FrameRenderer(){
 
 }
 
+
+void FrameRenderer::init(std::vector<VkImageView>& swapchainImageViews, Handle<TextureAttachment> input){
+    VkFormat displayFormat = m_renderer->getDisplay().getSwapchainFormat();
+
+    // render pass
+    m_renderPassLayout = m_rm->create<RenderPassLayout>(RenderPassLayoutDesc{
+        .colorAttatchmentFormats = {displayFormat},
+        .subpass = {
+            .colorAttachments = {0}
+        }
+    });
+
+    m_renderPass = m_rm->create<RenderPass>(RenderPassDesc{
+        .dimensions = m_dim,
+        .layout = m_renderPassLayout,
+        .colorAttachments = {
+            {
+                .swapchainImageViews = swapchainImageViews,
+                .finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+            }
+        }
+    });
+
+    // descriptor set layout
+    m_descriptorSetLayout = m_rm->create<DescriptorSetLayout>(DescriptorSetLayoutDesc{
+        .textures = {
+            {.binding = 0}
+        }
+    });
+
+    // shader
+    m_shader = m_rm->create<Shader>(ShaderDesc{
+        .vertexShader = {.path = ""},
+        .fragmentShader = {.path = ""},
+        .descriptorSets = {
+            { }, // unused
+            { }, // unused
+            { }, // unused
+            { m_descriptorSetLayout }
+        },
+        .graphicsState = {
+            .renderPass = m_renderPass,
+        }
+    });
+
+    // descriptor set
+    TextureAttachment* attachment = m_rm->get<TextureAttachment>(input);
+    m_descriptorSet = m_rm->create<DescriptorSet>(DescriptorSetDesc{
+        .textures = {
+            {.textures = attachment->textures}
+        },
+        .layout = m_descriptorSetLayout
+    });
+}
+
+
 void FrameRenderer::render(){
+
+}
+
+
+void FrameRenderer::destroy(){
 
 }
 
--- src/render/vulkan/FrameRenderer.h
+++ src/render/vulkan/FrameRenderer.h
@@ -1,22 +1,30 @@
 #pragma once
 
 #include "VulkanRenderer.h"
+#include "resource/ResourceTypes.h"
 #include "resource/VulkanResourceManager.h"
 
 namespace spr::gfx {
 class FrameRenderer {
 public:
-    FrameRenderer(VulkanResourceManager& rm, VkFormat displayFormat, glm::vec3 dimensions, Handle<Texture> result);
+    FrameRenderer();
+    FrameRenderer(VulkanResourceManager& rm, VulkanRenderer& renderer, glm::uvec3 dimensions);
     ~FrameRenderer();
 
-    RenderFrame& getCurrentFrame();
+
+    void init(std::vector<VkImageView>& swapchainImages, Handle<TextureAttachment> input);
     void render();
-    void destroy(VulkanResourceManager& rm);
+    void destroy();
 
 private:
+    VulkanResourceManager* m_rm;
+    VulkanRenderer* m_renderer;
+    glm::uvec3 m_dim;
+
     Handle<RenderPassLayout> m_renderPassLayout;
     Handle<RenderPass> m_renderPass;
+    Handle<DescriptorSetLayout> m_descriptorSetLayout;
+    Handle<DescriptorSet> m_descriptorSet;
     Handle<Shader> m_shader;
-    Handle<Texture> result;
 };
 }
