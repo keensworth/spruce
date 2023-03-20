b62dc7b9d9fd2788f18e321dea04dd815c7ed55a
s3rgei <benjakeen@gmail.com>
2023-03-19 20:50:29 -0500

    abstraction for binding new/incompatible descriptor sets

---
+++ src/render/vulkan/DescriptorSetHandler.cpp
@@ -0,0 +1,48 @@
+#include "DescriptorSetHandler.h"
+#include <vulkan/vulkan_core.h>
+
+namespace spr::gfx {
+
+DescriptorSetHandler::DescriptorSetHandler(){
+
+}
+
+DescriptorSetHandler::DescriptorSetHandler(VulkanResourceManager* rm, VkCommandBuffer commandBuffer){
+    m_rm = rm;
+    m_commandBuffer = commandBuffer;
+
+    reset();
+}
+
+DescriptorSetHandler::~DescriptorSetHandler(){
+
+}
+
+void DescriptorSetHandler::set(uint32 set, Handle<DescriptorSet> handle){
+    if (set >= 4){
+        SprLog::warn("DescriptorSetHandler: Set does not exist, set must be < 4");
+        return;
+    }
+    
+    if (!(handle == m_sets[set]))
+        m_sets[set] = handle;
+}
+
+void DescriptorSetHandler::updateBindings(VkPipelineLayout pipelineLayout, uint32 frameIndex){
+    for (uint32 i = 0; i < 4; i++){
+        if (m_sets[i].isValid()){
+            DescriptorSet* descSet = m_rm->get<DescriptorSet>(m_sets[i]);
+            VkDescriptorSet set = descSet->global ? descSet->descriptorSets[0] : descSet->descriptorSets[frameIndex];
+            vkCmdBindDescriptorSets(m_commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, i, 1, &set, 0, NULL);
+        }
+    }
+}
+
+void DescriptorSetHandler::reset(){
+    m_sets[0] = Handle<DescriptorSet>();
+    m_sets[1] = Handle<DescriptorSet>();
+    m_sets[2] = Handle<DescriptorSet>();
+    m_sets[3] = Handle<DescriptorSet>();
+}
+
+}
---
+++ src/render/vulkan/DescriptorSetHandler.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vulkan/vulkan_core.h>
+#include "resource/ResourceTypes.h"
+#include "../scene/Draw.h"
+#include "resource/VulkanResourceManager.h"
+
+namespace spr::gfx{
+
+class DescriptorSetHandler{
+public:
+    DescriptorSetHandler();
+    DescriptorSetHandler(VulkanResourceManager* rm, VkCommandBuffer commandBuffer);
+    ~DescriptorSetHandler();
+
+    void set(uint32 set, Handle<DescriptorSet> handle);
+    void updateBindings(VkPipelineLayout pipelineLayout, uint32 frameIndex);
+    void reset();
+
+private:
+    VulkanResourceManager* m_rm;
+    VkCommandBuffer m_commandBuffer;
+    
+    Handle<DescriptorSet> m_sets[4];
+};
+}
