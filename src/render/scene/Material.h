c466782076089dfed6d1cef9dad82a91f0616a8d
s3rgei <benjakeen@gmail.com>
2023-04-02 16:04:10 -0500

    add default material parameters

--- src/render/scene/Material.h
+++ src/render/scene/Material.h
@@ -23,7 +23,25 @@ typedef enum : uint32 {
 } MaterialFlags;
 
 typedef struct MaterialData {
+    uint32 baseColorTexIdx    = 0; // default_color
+    uint32 metalRoughTexIdx   = 1; // default_input_black
+    uint32 normalTexIdx       = 1; // default_input_black
+    uint32 occlusionTexIdx    = 1; // default_input_black
 
+    uint32 emissiveTexIdx     = 1; // default_input_black
+    float metallicFactor      = 1;
+    float roughnessFactor     = 1;
+    float normalScale         = 1;
+
+    glm::vec4 baseColorFactor = {1.f,1.f,1.f,1.f};
+
+    glm::vec3 emissiveFactor  = {0.f,0.f,0.f};
+    float occlusionStrength   = 1;
+
+    uint32 alpha      = 0;
+    float alphaCutoff = 0.5f;
+    uint32 flags      = MTL_NONE;
+    uint32 pad;
 } MaterialData;
 
 }
