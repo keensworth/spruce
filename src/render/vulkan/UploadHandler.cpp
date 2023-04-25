04beeda07eb7f362fe22f85d98cfb56d3973a827
s3rgei <benjakeen@gmail.com>
2023-04-24 21:10:57 -0500

    tweak additions to transfer queues, add method for separately performing graphics barriers

--- src/render/vulkan/UploadHandler.cpp
+++ src/render/vulkan/UploadHandler.cpp
@@ -2,6 +2,7 @@
 #include "GPUStreamer.h"
 #include "resource/ResourceTypes.h"
 #include "resource/VulkanResourceManager.h"
+#include "../../debug/SprLog.h"
 
 namespace spr::gfx {
 
@@ -9,24 +10,69 @@ UploadHandler::UploadHandler() {
     
 }
 
-UploadHandler::UploadHandler(VulkanDevice& device, VulkanResourceManager& rm, CommandBuffer& transferCommandBuffer, CommandBuffer& graphicsCommandBuffer)
-                            : m_streamer(device, rm, transferCommandBuffer, graphicsCommandBuffer) {
-    m_transferCommandBuffer = &transferCommandBuffer;
-    m_graphicsCommandBuffer = &graphicsCommandBuffer;
-    reset();
-}
-
 UploadHandler::~UploadHandler() {
-    if (m_destroyed)
+    if (m_destroyed || !m_initialized)
         return;
     
     SprLog::warn("[UploadHandler] [~] Calling destroy() in destructor");
     destroy();
 }
 
+UploadHandler& UploadHandler::operator=(UploadHandler&& other) noexcept{
+    if (this != &other) {
+        m_streamer = std::move(other.m_streamer);
+        m_frameId = other.m_frameId;
+        m_rm = other.m_rm;
+        m_transferCommandBuffer = other.m_transferCommandBuffer;
+        m_graphicsCommandBuffer = other.m_graphicsCommandBuffer;
+        m_bufferUploadQueue = std::move(other.m_bufferUploadQueue);
+        m_dynamicBufferUploadQueue = std::move(other.m_dynamicBufferUploadQueue);
+        m_textureUploadQueue = std::move(other.m_textureUploadQueue);
+        m_destroyed = other.m_destroyed;
+
+        other.m_frameId = 0;
+        other.m_rm = nullptr;
+        other.m_transferCommandBuffer = nullptr;
+        other.m_graphicsCommandBuffer = nullptr;
+        other.m_bufferUploadQueue.clear();
+        other.m_dynamicBufferUploadQueue.clear();
+        other.m_textureUploadQueue.clear();
+        other.m_destroyed = false;
+    }
+    return *this;
+}
+
+UploadHandler& UploadHandler::operator=(const UploadHandler& other) {
+    if (this != &other) {
+        m_streamer = other.m_streamer;
+        m_frameId = other.m_frameId;
+        m_rm = other.m_rm;
+        m_transferCommandBuffer = other.m_transferCommandBuffer;
+        m_graphicsCommandBuffer = other.m_graphicsCommandBuffer;
+        m_bufferUploadQueue = other.m_bufferUploadQueue;
+        m_dynamicBufferUploadQueue = other.m_dynamicBufferUploadQueue;
+        m_textureUploadQueue = other.m_textureUploadQueue;
+        m_destroyed = other.m_destroyed;
+    }
+    return *this;
+}
+
+void UploadHandler::init(VulkanDevice& device, VulkanResourceManager& rm, CommandBuffer& transferCommandBuffer, CommandBuffer& graphicsCommandBuffer){
+    m_rm = &rm;
+    m_transferCommandBuffer = &transferCommandBuffer;
+    m_graphicsCommandBuffer = &graphicsCommandBuffer;
+    reset();
+
+    m_streamer.init(device, rm, transferCommandBuffer, graphicsCommandBuffer);
+
+    m_initialized = true;
+}
+
 void UploadHandler::destroy(){
     m_streamer.destroy();
+    reset();
     m_destroyed = true;
+    SprLog::info("[UploadHandler] [destroy] destroyed...");
 }
 
 void UploadHandler::submit() {
@@ -34,12 +80,10 @@ void UploadHandler::submit() {
     for (GPUStreamer::BufferTransfer upload : m_bufferUploadQueue) {
         m_streamer.transfer(upload);
     }
-
     // upload dynamic buffers (N-buffered)
     for (GPUStreamer::BufferTransfer upload : m_dynamicBufferUploadQueue) {
         m_streamer.transferDynamic(upload, m_frameId);
     }
-
     // upload textures/images
     for (GPUStreamer::TextureTransfer upload : m_textureUploadQueue) {
         m_streamer.transfer(upload);
@@ -48,11 +92,10 @@ void UploadHandler::submit() {
     // flush uploads and submit transfer command buffer
     m_streamer.flush();
     m_transferCommandBuffer->submit();
+}
 
-    // prepare gfx resource barriers (but don't submit)
-    m_graphicsCommandBuffer->begin();
+void UploadHandler::performGraphicsBarriers(){
     m_streamer.performGraphicsBarriers();
-    m_graphicsCommandBuffer->end();
 }
 
 void UploadHandler::setFrameId(uint32 frame){
@@ -66,6 +109,7 @@ void UploadHandler::reset() {
     m_dynamicBufferUploadQueue.reserve(32);
     m_textureUploadQueue = std::vector<GPUStreamer::TextureTransfer>();
     m_textureUploadQueue.reserve(32);
+    m_streamer.reset();
 }
 
 }
--- src/render/vulkan/UploadHandler.h
+++ src/render/vulkan/UploadHandler.h
@@ -6,41 +6,57 @@
 #include "resource/ResourceTypes.h"
 #include "../core/memory/TempBuffer.h"
 #include "GPUStreamer.h"
-#include <vulkan/vulkan_core.h>
+#include "../../external/volk/volk.h"
 
 namespace spr::gfx {
 
 class UploadHandler{
 public:
     UploadHandler();
-    UploadHandler(VulkanDevice& device, VulkanResourceManager& rm, CommandBuffer& transferCommandBuffer, CommandBuffer& graphicsCommandBuffer);
+    UploadHandler& operator=(UploadHandler&& other) noexcept;
+    UploadHandler& operator=(const UploadHandler& other);
     ~UploadHandler();
+
+
+
 
     template <typename T>
     void uploadBuffer(TempBuffer<T>& src, Handle<Buffer> dst) {
-        m_bufferUploadQueue.push_back({
-            .pSrc = src.getData(),
-            .size = src.getSize(),
-            .dst = dst
-        });
+        if (src.getSize() == 0)
+            return;
+        Buffer* dstBuffer = m_rm->get<Buffer>(dst);
+        GPUStreamer::BufferTransfer transfer = {
+            .pSrc = (unsigned char*)src.getBytes(),
+            .size = (uint32)src.getSizeBytes(),
+            .dst = dstBuffer
+        };
+        m_bufferUploadQueue.push_back(transfer);
     }
 
     template <typename T>
     void uploadDyanmicBuffer(TempBuffer<T>& src, Handle<Buffer> dst) {
-        m_dynamicBufferUploadQueue.push_back({
-            .pSrc = src.getData(),
-            .size = src.getSize(),
-            .dst = dst
-        });
+        if (src.getSize() == 0)
+            return;
+        Buffer* dstBuffer = m_rm->get<Buffer>(dst);
+        GPUStreamer::BufferTransfer transfer = {
+            .pSrc = (unsigned char*)src.getBytes(),
+            .size = (uint32)(src.getSize() * sizeof(T)),
+            .dst = dstBuffer
+        };
+        m_dynamicBufferUploadQueue.push_back(transfer);
     }
 
     template <typename T>
     void uploadTexture(TempBuffer<T>& src, Handle<Texture> dst) {
-        m_textureUploadQueue.push_back({
-            .pSrc = src.getData(),
-            .size = src.getSize(),
-            .dst = dst
-        });
+        if (src.getSize() == 0)
+            return;
+        Texture* dstTexture = m_rm->get<Texture>(dst);
+        GPUStreamer::TextureTransfer transfer = {
+            .pSrc = (unsigned char*)src.getBytes(),
+            .size = (uint32)(src.getSize() * sizeof(T)),
+            .dst = dstTexture
+        };
+        m_textureUploadQueue.push_back(transfer);
     }
 
     void submit();
@@ -50,6 +66,7 @@ private:
     uint32 m_frameId;
 
     // non-owning
+    VulkanResourceManager* m_rm;
     CommandBuffer* m_transferCommandBuffer;
     CommandBuffer* m_graphicsCommandBuffer;
 
@@ -57,11 +74,14 @@ private:
     std::vector<GPUStreamer::BufferTransfer> m_dynamicBufferUploadQueue;
     std::vector<GPUStreamer::TextureTransfer> m_textureUploadQueue;
 
+    bool m_initialized = false;
     bool m_destroyed = false;
 
     void setFrameId(uint32 frameId);
+    void init(VulkanDevice& device, VulkanResourceManager& rm, CommandBuffer& transferCommandBuffer, CommandBuffer& graphicsCommandBuffer);
     void reset();
     void destroy();
+    void performGraphicsBarriers();
 
     friend class VulkanRenderer;
 };
