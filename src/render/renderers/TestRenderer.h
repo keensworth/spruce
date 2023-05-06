aa6574fc91c9a150c7ad216604a8b9fa4c97f54b
s3rgei <benjakeen@gmail.com>
2023-05-06 05:57:05 -0500

    clean up resource creation, remove unused descriptor sets

--- src/render/renderers/TestRenderer.h
+++ src/render/renderers/TestRenderer.h
@@ -22,18 +22,13 @@ public:
 
     void init(
         Handle<DescriptorSet> globalDescriptorSet, 
-        Handle<DescriptorSetLayout> globalDescSetLayout,
-        Handle<DescriptorSet> frameDescriptorSets[MAX_FRAME_COUNT],
-        Handle<DescriptorSetLayout> frameDescriptorSetLayout)
+        Handle<DescriptorSetLayout> globalDescSetLayout)
     {
         m_globalDescriptorSet = globalDescriptorSet;
         m_globalDescriptorSetLayout = globalDescSetLayout;
-        for (uint32 i = 0; i < MAX_FRAME_COUNT; i++)
-            m_descriptorSets[i] = frameDescriptorSets[i];
-        m_descriptorSetLayout = frameDescriptorSetLayout;
 
         // color attachment
-        m_attachment = m_rm ->create<TextureAttachment>(TextureAttachmentDesc{
+        m_attachment = m_rm ->create<TextureAttachment>({
             .textureLayout = {
                 .dimensions = m_dim,
                 .format = Flags::Format::RGBA8_UNORM,
@@ -44,13 +39,13 @@ public:
         });
 
         // render pass
-        m_renderPassLayout = m_rm->create<RenderPassLayout>(RenderPassLayoutDesc{
+        m_renderPassLayout = m_rm->create<RenderPassLayout>({
             .colorAttatchmentFormats = {Flags::RGBA8_UNORM},
             .subpass = {
                 .colorAttachments = {0}
             }
         });
-        m_renderPass = m_rm->create<RenderPass>(RenderPassDesc{
+        m_renderPass = m_rm->create<RenderPass>({
             .dimensions = m_dim,
             .layout = m_renderPassLayout,
             .colorAttachments = {
@@ -62,14 +57,14 @@ public:
         });
 
         // shader
-        m_shader = m_rm->create<Shader>(ShaderDesc{
-            .vertexShader = {.path = "../data/shaders/test.vert.spv"},
-            .fragmentShader = {.path = "../data/shaders/test.frag.spv"},
+        m_shader = m_rm->create<Shader>({
+            .vertexShader = {.path = "../data/shaders/spv/test.vert.spv"},
+            .fragmentShader = {.path = "../data/shaders/spv/test.frag.spv"},
             .descriptorSets = {
                 { globalDescSetLayout },
                 { }, // unused
-                { m_descriptorSetLayout },
-                { } // unused
+                { }, // unused
+                { }  // unused
             },
             .graphicsState = {
                 .renderPass = m_renderPass,
@@ -80,7 +75,7 @@ public:
 
 
     void render(CommandBuffer& cb, BatchManager& batchManager){
-        RenderPassRenderer& passRenderer = cb.beginRenderPass(m_renderPass, glm::vec4(1.0f,0.f,0.f,1.f));
+        RenderPassRenderer& passRenderer = cb.beginRenderPass(m_renderPass, glm::vec4(0.0f,1.f,0.f,1.f));
         
         passRenderer.drawSubpass(
             {.shader = m_shader, .set0 =  m_globalDescriptorSet}, 
@@ -110,8 +105,6 @@ private: // owning
     Handle<TextureAttachment> m_attachment;
     Handle<RenderPassLayout> m_renderPassLayout;
     Handle<RenderPass> m_renderPass;
-    Handle<DescriptorSetLayout> m_descriptorSetLayout;
-    Handle<DescriptorSet> m_descriptorSets[MAX_FRAME_COUNT];
     Handle<Shader> m_shader;
 
 private: // non-owning
