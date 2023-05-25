c12817b351955e82f393ea2ce7c1fc38d47b6fd1
s3rgei <benjakeen@gmail.com>
2023-05-25 10:20:52 -0500

    use mouse to control camera, add additional models to scene

--- data/shaders/pbr_mesh.frag
+++ data/shaders/pbr_mesh.frag
@@ -100,7 +100,6 @@ void main(){
 			float d = length(lights[i].pos - pos.rgb);
 			attenuation = (clamp(lights[i].range - d, 0.0, lights[i].range)) / (1.0 + 0.5*d*d);
 		}
-        float d = length(lights[i].pos - pos.rgb);
         vec3 Lradiance = lights[i].color * attenuation * lights[i].intensity;
 
 		// Half-vector between Li and Lo.
@@ -134,35 +133,10 @@ void main(){
 
 	vec3 ambientLighting;
 	{
-		// // Sample diffuse irradiance at normal direction.
-		// vec3 irradiance = texture(irradianceTexture, N).rgb;
-
-		// // Calculate Fresnel term for ambient lighting.
-		// // Since we use pre-filtered cubemap(s) and irradiance is coming from many directions
-		// // use cosLo instead of angle with light's half-vector (cosLh above).
-		// // See: https://seblagarde.wordpress.com/2011/08/17/hello-world/
-		// vec3 F = fresnelSchlick(F0, cosLo);
-
-		// // Get diffuse contribution factor (as with direct lighting).
-		// vec3 kd = mix(vec3(1.0) - F, vec3(0.0), metalness);
-
-		// // Irradiance map contains exitant radiance assuming Lambertian BRDF, no need to scale by 1/PI here either.
-		// vec3 diffuseIBL = kd * albedo * irradiance;
-
-		// // Sample pre-filtered specular reflection environment at correct mipmap level.
-		// int specularTextureLevels = textureQueryLevels(specularTexture);
-		// vec3 specularIrradiance = textureLod(specularTexture, Lr, roughness * specularTextureLevels).rgb;
-
-		// // Split-sum approximation factors for Cook-Torrance specular BRDF.
-		// vec2 specularBRDF = texture(specularBRDF_LUT, vec2(cosLo, roughness)).rg;
-
-		// // Total specular IBL contribution.
-		// vec3 specularIBL = (F0 * specularBRDF.x + specularBRDF.y) * specularIrradiance;
 
 		vec3 diffuseIBL = vec3(0.0,0.0,0.0);
 		vec3 specularIBL = vec3(0.0,0.0,0.0);
 
-		// Total ambient lighting contribution.
 		ambientLighting = diffuseIBL + specularIBL + mapEmissive;
 	}
     
--- main.cpp
+++ main.cpp
@@ -2,59 +2,72 @@
 #include "data/asset_ids.h"
 #include "glm/ext/quaternion_trigonometric.hpp"
 #include "glm/ext/scalar_constants.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include "src/interface/Window.h"
 #include "src/render/SprRenderer.h"
 #include "src/render/scene/Material.h"
+#include "src/render/scene/SceneData.h"
 #include "src/resource/ResourceTypes.h"
 #include "src/resource/SprResourceManager.h"
 #include "src/debug/SprLog.h"
 #include <chrono>
 #include <thread>
 #include "glm/gtx/string_cast.hpp"
+#include <glm/gtx/vector_angle.hpp>
 #include <chrono>
 
 using namespace spr;
 
-void handleInput(InputManager& input, glm::vec3& pos, glm::vec3& dir, glm::vec3& up, glm::vec3& right){
-    if (input.isKeyDown(spr::SPR_w)){
-        pos += 0.12f*dir;
+void handleInput(InputManager& input, vec3& pos, vec3& dir, vec3& up, vec3& right, spr::Window& window, float dt){
+    if (input.isKeyDownEdge(SPR_TAB)){
+        window.setRelativeMouse(!window.isRelativeMouse());
     }
-    if (input.isKeyDown(spr::SPR_s)){
-        pos += -0.12f*dir;
+    
+    vec3 zAxis = {0.f, 0.f, 1.f};
+
+    float vel = 4.0f;
+    if (input.isKeyDown(SPR_LSHIFT)){
+        vel *= 1.75f;
     }
-    if (input.isKeyDown(spr::SPR_a)){
-        pos += -0.12f*right;
+    if (input.isKeyDown(SPR_LCTRL)){
+        vel *= 0.75f;
     }
-    if (input.isKeyDown(spr::SPR_d)){
-        pos += 0.12f*right;
+    if (input.isKeyDown(SPR_w)){
+        pos +=  dt * vel * dir;
     }
-    if (input.isKeyDown(spr::SPR_SPACE)){
-        pos += vec3{0.f, 0.f, 0.04f};
+    if (input.isKeyDown(SPR_s)){
+        pos += -1.f * dt * vel * dir;
     }
-    if (input.isKeyDown(spr::SPR_LSHIFT)){
-        pos += vec3{0.f, 0.f, -0.12f};
+    if (input.isKeyDown(SPR_a)){
+        pos += -1.f * dt * vel * right;
+    }
+    if (input.isKeyDown(SPR_d)){
+        pos +=  dt * vel * right;
+    }
+    if (input.isKeyDown(SPR_SPACE)){
+        pos +=  dt * vel * zAxis;
+    }
+    
+    float yaw = 0.f;
+    float pitch = 0.f;
+    if (window.isRelativeMouse()){
+        yaw   = -(float)input.getMouseMotion().x * 0.0005f;
+        pitch = -(float)input.getMouseMotion().y * 0.0005f;
     }
 
-    float yaw = 0;
-    if (input.isKeyDown(spr::SPR_LEFT)){
-        yaw = 0.02;
-    } else if (input.isKeyDown(spr::SPR_RIGHT)){
-        yaw = -0.02;
-    }
+    // horizontal camera
+    dir = normalize(rotate(angleAxis(yaw, zAxis), dir));
+    right = normalize(rotate(angleAxis(yaw, zAxis), right));
 
-    float pitch = 0;
-    if (input.isKeyDown(spr::SPR_UP)){
-        pitch = 0.02;
-    } else if (input.isKeyDown(spr::SPR_DOWN)){
-        pitch = -0.02;
-    }
-
-    // update camera
-    dir = rotate(angleAxis(yaw, vec3{0.f, 0.f, 1.f}), dir);
-    up = rotate(angleAxis(yaw, vec3{0.f, 0.f, 1.f}), up);
-    right = rotate(angleAxis(yaw, vec3{0.f, 0.f, 1.f}), right);
-    dir = rotate(angleAxis(pitch, right), dir);
-    up = rotate(angleAxis(pitch, right), up);
+    // vertical camera
+    vec3 temp = rotate(angleAxis(pitch, right), dir);
+    float dotZ = max(dot(temp, zAxis), dot(temp, -zAxis));
+    if (dotZ >= 0.97f)
+        dir = normalize(rotate(angleAxis(glm::acos(0.96999f)*sign(-dir.z), right), zAxis*sign(dir.z)));
+    else 
+        dir = temp;
+    up = normalize(cross(right, dir));
+    
 }
 
 
@@ -62,6 +75,7 @@ int main() {
     // window and input
     spr::Window window = spr::Window(std::string("Spruce Test"), 1920, 1080);
     window.init();
+    window.setRelativeMouse(true);
     InputManager& input = window.getInputManager();
 
     // renderer and assets
@@ -80,12 +94,15 @@ int main() {
     auto stop = std::chrono::high_resolution_clock::now();
     auto printTime = std::chrono::high_resolution_clock::now();
     uint32 frame = 0;
+    double dt = 0.f;
 
     // simple game loop
     while (!input.isKeyDown(spr::SPR_ESCAPE)){
+        window.update();
+
         start = std::chrono::high_resolution_clock::now();
         
-        // insert models using their ids directly
+        //insert models using their ids directly
         renderer.insertModel(data::helmet, {
             .position = {0.f, 0.f, 0.f}, 
             .rotation = angleAxis((pi<float>()/2)*(frame/240.f), vec3{1.f, 0.f, 0.f}), 
@@ -96,19 +113,29 @@ int main() {
             .rotation = angleAxis((pi<float>()/2), vec3{1.f, 0.f, 0.f}), 
             .scale = 0.2f
         });
+        renderer.insertModel(data::waterbottle, {
+            .position = {3.f, 0.f, 0.f}, 
+            .rotation = angleAxis(0.f, vec3{1.f, 0.f, 0.f}), 
+            .scale = 3.f
+        });
+        renderer.insertModel(data::boomboxwithaxes, {
+            .position = {-2.5f, 0.f, -1.f}, 
+            .rotation = angleAxis(pi<float>()/2.f, vec3{1.f, 0.f, 0.f}), 
+            .scale = 10.f
+        });
         renderer.insertModel(data::sponza, {
             .position = {0.f, 0.f, -5.f}, 
             .rotation = angleAxis(pi<float>()/2, vec3{1.f, 0.f, 0.f}), 
-            .scale = 0.02f
+            .scale = 0.012f
         });
         
         // lights
-        renderer.insertLight({.pos = {3.f, 0.f, -1.f}, .intensity = 1.f, .range = 32.f});
-        renderer.insertLight({.pos = {20.f * glm::sin(frame/500.f), 0.f, -1.f}, .intensity = 1.f, .range = 32.f});
-        renderer.insertLight({.pos = {-3.f, 0.f, -1.f}, .intensity = 1.f, .range = 32.f});
+        renderer.insertLight({.pos = {3.f, 0.f, -1.f}, .intensity = 1.f, .range = 16.f}); // point
+        renderer.insertLight({.pos = {-3.f, 0.f, -1.f}, .intensity = 1.f, .range = 16.f});// point
+        renderer.insertLight({.intensity = 1.2f, .dir = {0.2f, 0.f, -1.f}, .type = gfx::DIRECTIONAL});
 
         // handle input + update camera
-        handleInput(input, pos, dir, up, right);
+        handleInput(input, pos, dir, up, right, window, dt);
         renderer.updateCamera({.pos = pos, .dir = dir, .up = up});
 
         // render 
@@ -117,15 +144,18 @@ int main() {
         // timing + window update
         stop = std::chrono::high_resolution_clock::now();
         auto printDur = (stop - printTime);
+        auto dur = (stop - start);
+        double fps = (double)(1000000000)/dur.count();
+        dt = (double)dur.count()/(1000000000);
+
+        // print stats
         if (printDur.count() > 1000000000){
             printTime = std::chrono::high_resolution_clock::now();
-            auto dur = (stop - start);
             SprLog::info("[MAIN] frame: ", frame);
-            SprLog::info("[MAIN] fps: " + std::to_string((float)(1000000000)/dur.count()) );
+            SprLog::info("[MAIN] fps: " + std::to_string(fps) );
+            SprLog::info("[MAIN] dt: " + std::to_string(dt) );
         }
-        window.update();
         frame++;
-
     }
 
     return 1;
