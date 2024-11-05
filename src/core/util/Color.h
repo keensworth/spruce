a8e1c4e5793cb34833a579386bcb1a9030f731eb
s3rgei <benjakeen@gmail.com>
2024-11-05 10:38:48 -0600

    represent colors as rgb32, for now

--- src/core/util/Color.h
+++ src/core/util/Color.h
@@ -1,17 +1,45 @@
 #pragma once
-
 #include "spruce_core.h"
 
 namespace spr::color {
 
-static const glm::uvec3 white = {255, 255, 255};
-static const glm::uvec3 black = {  0,   0,   0};
-static const glm::uvec3 debug = {255,  51, 153};
-static const glm::uvec3 info  = {192, 192, 192};
-static const glm::uvec3 warn  = {255, 153,  51};
-static const glm::uvec3 error = {255,  51,  51};
-static const glm::uvec3 fatal = {255,  51,  51};
-static const glm::uvec3 value = { 79, 119,  45};
-static const glm::uvec3 time  = {  0, 153,  76};
+typedef uvec3 rgb32;
+
+static const rgb32 white = {255, 255, 255};
+static const rgb32 black = {  0,   0,   0};
+static const rgb32 debug = {255,  51, 153};
+static const rgb32 info  = {192, 192, 192};
+static const rgb32 warn  = {255, 153,  51};
+static const rgb32 error = {255,  51,  51};
+static const rgb32 fatal = {255,  51,  51};
+static const rgb32 value = {212, 224, 155};
+static const rgb32 time  = {  0, 153,  76};
+static const rgb32 label = {153, 217, 140};
+
+enum id {
+    WHITE,
+    BLACK,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL,
+    VALUE,
+    TIME,
+    LABEL
+};
+
+static const rgb32 idMap[10] = {
+    white,
+    black,
+    debug,
+    info,
+    warn,
+    error,
+    fatal,
+    value,
+    time,
+    label
+};
 
 }
