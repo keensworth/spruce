ad93a81c47d5da7a5122097bd880e8559b74598a
s3rgei <benjakeen@gmail.com>
2024-11-06 02:06:42 -0600

    add constructors for const arrays, const ptrs

--- src/core/util/Span.h
+++ src/core/util/Span.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "../spruce_core.h"
-#include "../../debug/SprLog.h"
 #include <initializer_list>
 #include <vector>
 #include <array>
+#include "spruce_core.h"
+#include "debug/SprLog.h"
 
 namespace spr {
 
@@ -16,22 +16,25 @@ class Span {
 public:
     Span() :  m_data(nullptr), m_size(0) {}
     Span(T* data, uint32 size) : m_data(data), m_size(size) {}
+    Span(const T* data, uint32 size) : m_data(data), m_size(size) {}
     Span(std::initializer_list<T> ilist) : m_data(ilist.begin()), m_size(ilist.size()) {}
     Span(std::vector<T>& vector) : m_data(vector.data()), m_size(vector.size()) {}
     template <std::size_t N>
     Span(std::array<T, N>& array) : m_data(array.data()), m_size(N) {}
+    template <std::size_t N>
+    Span(const T (&data)[N]) : m_data(data), m_size(N) {}
     Span(TempBuffer<T>& buffer) : m_data(buffer.getData()), m_size(buffer.getSize()) {}
 
     T operator[](uint32 index) {
         if (index >= m_size)
-            SprLog::error("[Span] [operator[]] index out of range for span size of ", m_size);
+            SprLog::error({{"[Span] ", color::GRADIENT17}, {"[operator[]] index "}, {index}, {" out of range for span size of "}, {m_size}});
         
         return m_data[index];
     }
 
     const T& operator[](uint32 index) const {
         if (index >= m_size)
-            SprLog::error("[Span] [const operator[]] index out of range for span size of ", m_size);
+            SprLog::error({{"[Span] ", color::GRADIENT17}, {"[const operator[]] index "}, {index}, {" out of range for span size of "}, {m_size}});
         
         return m_data[index];
     }
